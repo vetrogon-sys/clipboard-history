a82d4f08c7e5c016a631d3bdc29a49df557a64a1
kostya <kostyabogdanov338@gmail.com>
2025-12-21 11:34:24 +0300

    simple listener implementation
    Signed-off-by: kostya <kostyabogdanov338@gmail.com>

--- crates/clipboard/src/lib.rs
+++ crates/clipboard/src/lib.rs
@@ -1,14 +1,4 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+pub mod listener;
+pub mod x11;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use listener::ClipboardListener;
